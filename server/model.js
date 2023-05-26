c9f27512d3625239206ab53b6a4d31f571ec9ab7
Julie Krav <j.krav07@gmail.com>
2023-05-26 15:42:31 -0700

    GET request v2

--- server/model.js
+++ server/model.js
@@ -6,9 +6,27 @@ models.retrieveReviews = (productId, page, count, sort) => {
   const args = [productId, page, count, sort];
   let query;
   if (sort === "newest") {
-    query = 'SELECT Review_id, rating, date, summary, body, recommend, reviewer_name, response, helpfulness FROM Reviews WHERE Product_id=$1 ORDER BY date DESC LIMIT $3';
+    query = `SELECT r.Review_id, rating, date, summary, body, recommend, reviewer_name, response, helpfulness,
+    json_agg(json_build_object(
+      'id', ReviewPhoto_id,
+      'url', reviewPhoto_url)) as photos
+    FROM Reviews r
+    JOIN ReviewPhotos rp ON r.Review_id=rp.Review_id
+    WHERE Product_id=$1
+    GROUP BY r.Review_id
+    ORDER BY date DESC
+    LIMIT $3`;
   } else if (sort === "helpful") {
-    query = 'SELECT Review_id, rating, date, summary, body, recommend, reviewer_name, response, helpfulness FROM Reviews WHERE Product_id=$1 ORDER BY helpfulness DESC LIMIT $3';
+    query = `SELECT r.Review_id, rating, date, summary, body, recommend, reviewer_name, response, helpfulness,
+    json_agg(json_build_object(
+      'id', ReviewPhoto_id,
+      'url', reviewPhoto_url)) as photos
+    FROM Reviews r
+    JOIN ReviewPhotos rp ON r.Review_id=rp.Review_id
+    WHERE Product_id=$1
+    GROUP BY r.Review_id
+    ORDER BY helpfulness DESC
+    LIMIT $3`;
   }
   return db
     .any(query, args)
@@ -19,7 +37,6 @@ models.retrieveReviews = (productId, page, count, sort) => {
       console.log('ERROR RETRIEVING REVIEWS:', err);
     })
 }
-
 
 
 
